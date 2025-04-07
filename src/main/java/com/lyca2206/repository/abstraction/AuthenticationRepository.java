b5ac0cdf5cead674bb882ac43890cbb1f6a7d900
Ariel Eduardo Pabón Bolaños <lyca2206@gmail.com>
2025-04-07 14:51:31 -0500

    Feat: Implemented simple logic in MemoryAuthentication for sign in

--- src/main/java/com/lyca2206/repository/abstraction/AuthenticationRepository.java
+++ src/main/java/com/lyca2206/repository/abstraction/AuthenticationRepository.java
@@ -2,10 +2,12 @@ package com.lyca2206.repository.abstraction;
 
 import com.lyca2206.model.User;
 
+import javax.security.auth.login.AccountNotFoundException;
+import javax.security.auth.login.FailedLoginException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
 public interface AuthenticationRepository {
-    User signIn(User user);
+    User signIn(User user) throws AccountNotFoundException, FailedLoginException;
     void signUp(User user) throws NoSuchAlgorithmException, InvalidKeySpecException;
 }
--- src/main/java/com/lyca2206/repository/implementation/InMemoryAuthentication.java
+++ src/main/java/com/lyca2206/repository/implementation/InMemoryAuthentication.java
@@ -4,6 +4,8 @@ import com.lyca2206.model.User;
 import com.lyca2206.repository.abstraction.AuthenticationRepository;
 import com.lyca2206.utilities.hash.generator.HashGenerator;
 
+import javax.security.auth.login.AccountNotFoundException;
+import javax.security.auth.login.FailedLoginException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Map;
@@ -16,8 +18,18 @@ public class InMemoryAuthentication implements AuthenticationRepository {
     }
 
     @Override
-    public User signIn(User user) {
-        return null;
+    public User signIn(User user) throws AccountNotFoundException, FailedLoginException {
+        User storedUser = users.get(user.getEmail());
+
+        if (storedUser == null) {
+            throw new AccountNotFoundException("The specified user couldn't be found");
+        }
+
+        if (!storedUser.getPassword().equals(user.getPassword())) {
+            throw new FailedLoginException("The given credentials don't match");
+        }
+
+        return storedUser;
     }
 
     @Override
