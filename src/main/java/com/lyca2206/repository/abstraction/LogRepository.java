8c89f181bce33aa17fcbf6fefcc8d4a1f0b833c8
Ariel Eduardo Pabón Bolaños <lyca2206@gmail.com>
2025-04-07 09:09:57 -0500

    Feat: Added some methods to repository interfaces

--- src/main/java/com/lyca2206/repository/abstraction/LogRepository.java
+++ src/main/java/com/lyca2206/repository/abstraction/LogRepository.java
@@ -3,7 +3,10 @@ package com.lyca2206.repository.abstraction;
 import com.lyca2206.model.Log;
 import com.lyca2206.model.User;
 
+import java.util.List;
+
 public interface LogRepository {
     Log logWorkout(Log log);
+    List<Log> getLogs(User user);
     Log getLog(String name, User user);
 }
--- src/main/java/com/lyca2206/repository/abstraction/WorkoutRepository.java
+++ src/main/java/com/lyca2206/repository/abstraction/WorkoutRepository.java
@@ -2,7 +2,10 @@ package com.lyca2206.repository.abstraction;
 
 import com.lyca2206.model.Workout;
 
+import java.util.List;
+
 public interface WorkoutRepository {
     Workout createWorkout(Workout workout);
+    List<Workout> getWorkouts();
     Workout getWorkout(String name);
 }
